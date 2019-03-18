ba5481c09fd7712ae6647b54acbc991e142423b4
Arkadiy Ivanov <studiovasia18@gmail.com>
2019-03-18 17:43:47 +0300

    Добавленно MainActivityTwo (18.03.19)

--- app/src/main/java/com/example/oknapanorama/MainActivity.java
+++ app/src/main/java/com/example/oknapanorama/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.oknapanorama;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    //Определяем кнопку (ImageView) это лого.
+    private ImageView logoImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logoImage = /*(ImageView)*/ findViewById(R.id.logoImage);
+        logoImage.setOnClickListener(this);
+    }
+
+    // Пишем переход на другое активити (MainActivityTwo)
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.logoImage:
+                //Стартуем и переходим на другое активити (MainActivityTwo)
+                Intent intent = new Intent(this, MainActivityTwo.class);
+                //Стартуем
+                startActivity(intent);
+
+                break;
+            default:
+                break;
+        }
+
     }
 }
